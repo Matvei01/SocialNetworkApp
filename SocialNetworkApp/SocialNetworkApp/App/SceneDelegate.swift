175cbb9791acd3a26ade794c24bf9e2912d65e6d
Matvei <khlestov.matvei@mail.ru>
2024-04-08 22:33:28 +0500

    Created base project, removed storyboard references

---
+++ SocialNetworkApp/SocialNetworkApp/App/SceneDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  SceneDelegate.swift
+//  SocialNetworkApp
+//
+//  Created by Matvei Khlestov on 08.04.2024.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        let viewController = ViewController()
+        window.rootViewController = viewController
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
+
---
+++ SocialNetworkApp/SocialNetworkApp/Controllers/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  SocialNetworkApp
+//
+//  Created by Matvei Khlestov on 08.04.2024.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
+
